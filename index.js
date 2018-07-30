0e6fb7d511461d0a414be97c5872fe4b68eba1a7
Dee Race <deerace12@gmail.com>
2018-07-30 17:38:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-
-}
+  const newArray = [];
+  for (const user of drivers) {
+    if (user.toLowerCase() == name.toLowerCase()) {
+      newArray.push(user)
+    }
+  }
+  return newArray;
+};
